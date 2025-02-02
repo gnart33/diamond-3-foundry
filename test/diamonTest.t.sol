3145d8e663fd6555c54cd19704517ad5f3d628c6
gnart <nvtrang91@gmail.com>
2025-02-03 00:18:26 +0700

    add test

--- test/diamonTest.t.sol
+++ test/diamonTest.t.sol
@@ -1,18 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Test} from "forge-std/Test.sol";
-import {DeployDiamond} from "../script/deploy.s.sol";
+import {Test, console2} from "forge-std/Test.sol";
+// import {DeployDiamond} from "../script/deploy.s.sol";
+import {DiamondCutFacet} from "../contracts/facets/DiamondCutFacet.sol";
+import {OwnershipFacet} from "../contracts/facets/OwnershipFacet.sol";
+import {LibDiamond} from "../contracts/libraries/LibDiamond.sol";
+import {Diamond} from "../contracts/Diamond.sol";
+import {IDiamondCut} from "../contracts/interfaces/IDiamondCut.sol";
+import {DiamondLoupeFacet} from "../contracts/facets/DiamondLoupeFacet.sol";
 
 contract DiamondTest is Test {
-    DeployDiamond public deployDiamond;
+    // DeployDiamond public deployDiamond;
+    DiamondCutFacet public diamondCutFacet;
+    OwnershipFacet public ownershipFacet;
+    Diamond public diamond;
+    DiamondLoupeFacet public diamondLoupeFacet;
+    address public diamondOwner;
+    address[] public facets;
 
-    // function setUp() public {
-    //     deployDiamond = new DeployDiamond();
-    // }
+    // Diamond public diamond;
+    function setUp() public {
+        // deployDiamond = new DeployDiamond();
+        diamondOwner = address(123456);
 
-    function test_deployDiamond() public {
-        deployDiamond = new DeployDiamond();
-        deployDiamond.run();
+        diamondCutFacet = new DiamondCutFacet();
+        // diamondLoupeFacet = new DiamondLoupeFacet();
+        ownershipFacet = new OwnershipFacet();
+
+        diamond = new Diamond(address(diamondOwner), address(diamondCutFacet));
+    }
+
+    function test_diamondLoupeFacet() public {
+        diamondLoupeFacet = new DiamondLoupeFacet();
+
+        IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
+        bytes4[] memory functionSelectors = new bytes4[](1);
+
+        functionSelectors[0] = IDiamondCut.diamondCut.selector;
+        cut[0] = IDiamondCut.FacetCut({
+            facetAddress: address(diamondLoupeFacet),
+            action: IDiamondCut.FacetCutAction.Add,
+            functionSelectors: functionSelectors
+        });
+        diamondCutFacet.diamondCut(cut, address(0), "");
+
+        facets = diamondLoupeFacet.facetAddresses();
+        console2.log("Facets: ", facets.length);
     }
 }
